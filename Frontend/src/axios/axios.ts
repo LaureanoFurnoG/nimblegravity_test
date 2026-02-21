b836cc628a162319917c6ab2b1cb9ae613626c14
Laureano Furno <148632238+voxaive@users.noreply.github.com>
2026-02-21 00:30:34 -0300

    feat: added explicit content-type header & add more timeout

--- Frontend/src/axios/axios.ts
+++ Frontend/src/axios/axios.ts
@@ -1,8 +1,9 @@
-import axios from 'axios'
+import axios from "axios";
 
 const instanceAxios = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
-    timeout: 1000
-})
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 8000,
+  headers: { "Content-Type": "application/json" },
+});
 
-export default instanceAxios
+export default instanceAxios;
